6f649e733877deda2e1f443a7532f0e550683884
john mark padilla <padilla_j@f112.ub.lan>
2026-01-19 10:46:28 +0800

    Initial React project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Test from './Test'
+import Header from './Header'
+import Footer from './Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Test/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+
+    return (
+      <>
+        <h3>This is Footer Component</h3>
+      </>
+    )
+  }
+  
+  export default Footer
